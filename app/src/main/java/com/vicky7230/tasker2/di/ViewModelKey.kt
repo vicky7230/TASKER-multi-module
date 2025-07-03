a5cda4b79ea753776df532f80b68cc72aa06621b
Vipin <vicky7230@gmail.com>
2025-07-03 11:58:54 +0530

    improve multi binding in dagger

--- app/src/main/java/com/vicky7230/tasker2/di/ViewModelKey.kt
+++ app/src/main/java/com/vicky7230/tasker2/di/ViewModelKey.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import dagger.MapKey
 import kotlin.reflect.KClass
 
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
-@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
 @MapKey
-internal annotation class ViewModelKey(
+annotation class ViewModelKey(
     val value: KClass<out ViewModel>,
 )
